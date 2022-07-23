8d267d69c6230e6e7d736449a076d7dd38622b6a
AtlanticNova <69181762+AtlanticNova@users.noreply.github.com>
2022-07-24 00:30:32 +0700

    Update user_router.js

--- src/routes/user_router.js
+++ src/routes/user_router.js
@@ -6,6 +6,8 @@ routers.post("/users", user.login);
 routers.post("/users/register", user.register);
 routers.patch("/users/verification", user.verifyUser);
 // routers.post("/users/verification/resend", user.resendEmail); kirim ulang
-// routers.put("/users/edit", user.editUser)
+routers.post("/users/forgotPassword", user.forgotPassword);
+routers.patch("/users/resetPasword", user.resetPassword);
+routers.get("/users/keepLogin", user.keepLogin);
 
 module.exports = routers;
