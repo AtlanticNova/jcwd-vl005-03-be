748d7ca25ebdca6c17dac645ef798fd936b76bf6
Martinus Andika Novanawa <69181762+AtlanticNova@users.noreply.github.com>
2022-08-03 11:50:07 +0700

    Bug Fixing

--- src/helper/authentication.js
+++ src/helper/authentication.js
@@ -4,10 +4,15 @@ const createError = require('./create-error')
 
 module.exports = (req, resp, next) => {
     const token = req.header('authToken')
+    const userToken = req.header('authorization')
     console.log(`tokenadmin:`, token);
+    console.log(`tokenuser:`, userToken)
     try {
         //check token
         if(!token){
+            throw new createError(statusCode.UNAUTHORIZED, statusMessage.UNAUTHORIZED)
+        }
+        if(!userToken){
             throw new createError(statusCode.UNAUTHORIZED, statusMessage.UNAUTHORIZED)
         }
 
@@ -15,8 +20,12 @@ module.exports = (req, resp, next) => {
         const {email} = jwt.verify(token, process.env.SECRET_KEY)
         console.log('email when keep login:', email)
 
+        const {userEmail} = jwt.verify(userToken, process.env.SECRET_KEY)
+        console.log('user email wheen keep login:', userEmail)
+
         //modified object request
         req.email = email
+        req.userEmail = userEmail
         
         //send to controller
         next()
